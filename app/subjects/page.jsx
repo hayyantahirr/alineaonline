8a3ead36fe343a5917f4a9fa3e63a4faed467c69
hayyantahirr <hayyantahir7@gmail.com>
2026-08-12 01:14:10 +0500

    Pricing section added

--- app/subjects/page.jsx
+++ app/subjects/page.jsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { CheckCircle2, ArrowRight } from "lucide-react";
+import { ArrowRight } from "lucide-react";
 
 export default function SubjectsPage() {
   const subjects = [
